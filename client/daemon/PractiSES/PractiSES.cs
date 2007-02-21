f1bda3276545120572de9e508359a7f0c6bc0504
Can Berk Güder <cbguder@su.sabanciuniv.edu>
2007-02-21 17:24:25 +0000

    Client initial commit

---
+++ client/daemon/PractiSES/PractiSES.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting;
+using System.Text;
+
+namespace PractiSES
+{
+    class PractiSES
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
